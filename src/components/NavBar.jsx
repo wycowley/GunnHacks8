9534f672535ae3d96bf5b149dfb2808f23405a63
Wyatt Cowley <wycowley@gmail.com>
2022-02-06 11:10:43 -0800

    push navbar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./components.css";
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+    let location = useLocation();
+    const [active, setActive] = useState(location.pathname);
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setActive("home");
+        } else if (location.pathname === "/groups") {
+            setActive("groups");
+        } else if (location.pathname === "/profile") {
+            setActive("profile");
+        }
+    }, [location]);
+    console.log(location.pathname);
     return (
-        <div class='navbar'>
+        <div className='navbar'>
             <div>
-                <Link to='/groups'>Groups</Link>
-            </div>
-            <div>
-                <Link to='/'>Home</Link>
-            </div>
-            <div>
-                <Link to='/profile'>Profile</Link>
+                <div>
+                    <Link to='/groups'>Groups</Link>
+                </div>
+                <div className='home-container'>
+                    <Link to='/'>Home</Link>
+                </div>
+                <div>
+                    <Link to='/profile'>Profile</Link>
+                </div>
             </div>
         </div>
     );
